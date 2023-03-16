e57ca471bc40af0604b66ba97a21e1f96fb7add1
Srinivas Annavarpu <srini@Srinivass-MacBook-Pro.local>
2023-03-16 00:03:24 -0700

    Added DbContext, Minimal Api Request, Precipitation Model

--- Cloudweather.Precipitation/Program.cs
+++ Cloudweather.Precipitation/Program.cs
@@ -1,5 +1,21 @@
+using Cloudweather.Precipitation.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>
+{
+    if(days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+    }
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation.Where(p => p.ZipCode == zip && p.CreatedOn > startDate).ToListAsync();
+
+    return Results.Ok(results);
+});
 
 app.Run();
